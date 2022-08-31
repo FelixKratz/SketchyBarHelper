ebc5d6971e438c83cb4b2ac5688c97d9bbd15fbc
Felix Kratz <felix.kratz@tu-dortmund.de>
2022-08-31 13:08:38 +0200

    always await response

--- sketchybar.h
+++ sketchybar.h
@@ -61,36 +61,32 @@ void mach_receive_message(mach_port_t port, struct mach_buffer* buffer, bool tim
   }
 }
 
-char* mach_send_message(mach_port_t port, char* message, uint32_t len, bool await_response) {
+char* mach_send_message(mach_port_t port, char* message, uint32_t len) {
   if (!message || !port) {
     return NULL;
   }
 
   mach_port_t response_port;
-  if (await_response) {
-    mach_port_name_t task = mach_task_self();
-    if (mach_port_allocate(task, MACH_PORT_RIGHT_RECEIVE,
-                                 &response_port          ) != KERN_SUCCESS) {
-      return NULL;
-    }
+  mach_port_name_t task = mach_task_self();
+  if (mach_port_allocate(task, MACH_PORT_RIGHT_RECEIVE,
+                               &response_port          ) != KERN_SUCCESS) {
+    return NULL;
+  }
 
-    if (mach_port_insert_right(task, response_port,
-                                     response_port,
-                                     MACH_MSG_TYPE_MAKE_SEND)!= KERN_SUCCESS) {
-      return NULL;
-    }
+  if (mach_port_insert_right(task, response_port,
+                                   response_port,
+                                   MACH_MSG_TYPE_MAKE_SEND)!= KERN_SUCCESS) {
+    return NULL;
   }
 
   struct mach_message msg = { 0 };
   msg.header.msgh_remote_port = port;
-  if (await_response) {
-    msg.header.msgh_local_port = response_port;
-    msg.header.msgh_id = response_port;
-    msg.header.msgh_bits = MACH_MSGH_BITS_SET(MACH_MSG_TYPE_COPY_SEND,
-                                              MACH_MSG_TYPE_MAKE_SEND_ONCE,
-                                              0,
-                                              MACH_MSGH_BITS_COMPLEX       );
-  }
+  msg.header.msgh_local_port = response_port;
+  msg.header.msgh_id = response_port;
+  msg.header.msgh_bits = MACH_MSGH_BITS_SET(MACH_MSG_TYPE_COPY_SEND,
+                                            MACH_MSG_TYPE_MAKE_SEND_ONCE,
+                                            0,
+                                            MACH_MSGH_BITS_COMPLEX       );
 
   msg.header.msgh_size = sizeof(struct mach_message);
   msg.msgh_descriptor_count = 1;
@@ -108,13 +104,11 @@ char* mach_send_message(mach_port_t port, char* message, uint32_t len, bool awai
            MACH_MSG_TIMEOUT_NONE,
            MACH_PORT_NULL              );
 
-  if (await_response) {
-    struct mach_buffer buffer = { 0 };
-    mach_receive_message(response_port, &buffer, true);
-    if (buffer.message.descriptor.address)
-      return (char*)buffer.message.descriptor.address;
-    mach_msg_destroy(&buffer.message.header);
-  }
+  struct mach_buffer buffer = { 0 };
+  mach_receive_message(response_port, &buffer, true);
+  if (buffer.message.descriptor.address)
+    return (char*)buffer.message.descriptor.address;
+  mach_msg_destroy(&buffer.message.header);
 
   return NULL;
 }
@@ -132,8 +126,7 @@ char* sketchybar(char* message) {
 
   char* response = mach_send_message(mach_get_bs_port(),
                                      formatted_message,
-                                     message_length + 1,
-                                     true               );
+                                     message_length + 1 );
 
   if (response) return response;
   else return (char*)"";
